9bd5b34a30ee0205e047ea1e9c89a51c709e59db
Juan Ortiz <ortizjuan2@gmail.com>
2017-09-20 23:00:24 -0500

    feat:	Adding tf for the waypoint updater
    Adding tf transformation for the waypoint updater

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -171,10 +171,12 @@ class WaypointUpdater(object):
         ## use next waypoints to fit polynomial
 
         wp_end = next_wp + 200
+        more_wps = 0
         if wp_end >= len(self.waypoints):
             wp_end = len(self.waypoints)
-
-        if next_wp == wp_end:
+            more_wps = 200 - (wp_end - next_wp)
+            rospy.logwarm("more waypoints needed %d" % (more_wps))
+        elif next_wp == wp_end:
             next_wp = 0
             wp_end = 200
 
@@ -186,6 +188,16 @@ class WaypointUpdater(object):
                 zc.append(newPose.pose.position.z)
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
                 return []
+
+        if more_wps != 0:
+            for i in range(more_wps):
+                try:
+                    newPose = self.tflistener.transformPose('/base_link', self.waypoints[i].pose)
+                    xc.append(newPose.pose.position.x)
+                    yc.append(newPose.pose.position.y)
+                    zc.append(newPose.pose.position.z)
+                except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                    return []
 
         ## convert to car frame
 
@@ -232,29 +244,113 @@ class WaypointUpdater(object):
             ypoint = fy(xpoint)
             x_add_on = xpoint
             ## rotate back to world frame
+            npose = PoseStamped()
+            npose.header.stamp = rospy.Time(0)
+            npose.header.frame_id = '/base_link'
+            npose.pose.position.x = xpoint
+            npose.pose.position.y = ypoint
+            npose.pose.position.z = 0.
+            npose.pose.orientation.x = 0.
+            npose.pose.orientation.y = 0.
+            npose.pose.orientation.z = math.atan2(ypoint, xpoint)
+
+            newPose = self.tflistener.transformPose('/world', npose)
+
             xref = xpoint
             yref = ypoint
             xpoint = xref * np.cos(yaw) - yref * np.sin(yaw)
             ypoint = xref * np.sin(yaw) + yref * np.cos(yaw)
             xpoint += posx
             ypoint += posy
+
+
+            rospy.loginfo("old pose: %3.4f, %3.4f, %3.4f" % (xpoint,
+                                            ypoint,
+                                            yaw))
+
+
+
+            rospy.loginfo("new pose: %3.4f, %3.4f, %3.4f" % (newPose.pose.position.x,
+                                            newPose.pose.position.y,
+                                            newPose.pose.orientation.z))
+            '''
+            [INFO] [1505965263.675728]: old pose: 1232.3648, 1188.3714, 0.0429
+            [INFO] [1505965263.676147]: new pose: 62.6939, -0.2726, -0.0043
+            [INFO] [1505965263.677297]: old pose: 1232.7002, 1188.3743, 0.0429
+            [INFO] [1505965263.677650]: new pose: 63.0291, -0.2842, -0.0045
+            [INFO] [1505965263.678792]: old pose: 1233.0357, 1188.3771, 0.0429
+            [INFO] [1505965263.679199]: new pose: 63.3644, -0.2958, -0.0047
+            [INFO] [1505965263.680272]: old pose: 1233.3711, 1188.3797, 0.0429
+            [INFO] [1505965263.680660]: new pose: 63.6996, -0.3076, -0.0048
+            [INFO] [1505965263.681770]: old pose: 1233.7066, 1188.3822, 0.0429
+            [INFO] [1505965263.682346]: new pose: 64.0349, -0.3194, -0.0050
+            [INFO] [1505965263.683203]: old pose: 1234.0421, 1188.3846, 0.0429
+            [INFO] [1505965263.683471]: new pose: 64.3702, -0.3314, -0.0051
+            [INFO] [1505965263.684647]: old pose: 1234.3775, 1188.3869, 0.0429
+            [INFO] [1505965263.685133]: new pose: 64.7054, -0.3436, -0.0053
+            [INFO] [1505965263.686531]: old pose: 1234.7130, 1188.3891, 0.0429
+            [INFO] [1505965263.687051]: new pose: 65.0407, -0.3558, -0.0055
+            [INFO] [1505965263.688472]: old pose: 1235.0485, 1188.3911, 0.0429
+            [INFO] [1505965263.689000]: new pose: 65.3760, -0.3682, -0.0056
+            [INFO] [1505965263.690336]: old pose: 1235.3840, 1188.3930, 0.0429
+            [INFO] [1505965263.690838]: new pose: 65.7112, -0.3806, -0.0058
+            [INFO] [1505965263.692205]: old pose: 1235.7195, 1188.3948, 0.0429
+            [INFO] [1505965263.692722]: new pose: 66.0465, -0.3932, -0.0060
+            [INFO] [1505965263.694077]: old pose: 1236.0550, 1188.3965, 0.0429
+            [INFO] [1505965263.694580]: new pose: 66.3817, -0.4060, -0.0061
+            [INFO] [1505965263.695901]: old pose: 1236.3905, 1188.3980, 0.0429
+            [INFO] [1505965263.696423]: new pose: 66.7170, -0.4188, -0.0063
+            [INFO] [1505965263.697721]: old pose: 1236.7260, 1188.3995, 0.0429
+            [INFO] [1505965263.698249]: new pose: 67.0523, -0.4318, -0.0064
+
+
+            [INFO] [1505965752.690799]: old pose: 1320.0167, 1184.4527, -0.0725
+            [INFO] [1505965752.691052]: new pose: 1320.0167, 1184.4527, 0.9993
+            [INFO] [1505965752.691727]: old pose: 1320.3503, 1184.4186, -0.0725
+            [INFO] [1505965752.692062]: new pose: 1320.3503, 1184.4186, 0.9993
+            [INFO] [1505965752.692710]: old pose: 1320.6838, 1184.3845, -0.0725
+            [INFO] [1505965752.693016]: new pose: 1320.6838, 1184.3845, 0.9993
+            [INFO] [1505965752.693680]: old pose: 1321.0174, 1184.3504, -0.0725
+            [INFO] [1505965752.693982]: new pose: 1321.0174, 1184.3504, 0.9993
+            [INFO] [1505965752.694692]: old pose: 1321.3510, 1184.3163, -0.0725
+            [INFO] [1505965752.695017]: new pose: 1321.3510, 1184.3163, 0.9993
+            [INFO] [1505965752.695732]: old pose: 1321.6845, 1184.2823, -0.0725
+            [INFO] [1505965752.696024]: new pose: 1321.6845, 1184.2823, 0.9993
+            [INFO] [1505965752.696716]: old pose: 1322.0181, 1184.2482, -0.0725
+            [INFO] [1505965752.697002]: new pose: 1322.0181, 1184.2482, 0.9993
+            [INFO] [1505965752.697638]: old pose: 1322.3517, 1184.2142, -0.0725
+            [INFO] [1505965752.697935]: new pose: 1322.3517, 1184.2142, 0.9993
+            [INFO] [1505965752.698572]: old pose: 1322.6852, 1184.1802, -0.0725
+            [INFO] [1505965752.698866]: new pose: 1322.6852, 1184.1802, 0.9993
+            [INFO] [1505965752.699549]: old pose: 1323.0188, 1184.1462, -0.0725
+            [INFO] [1505965752.699922]: new pose: 1323.0188, 1184.1462, 0.9993
+            [INFO] [1505965752.700683]: old pose: 1323.3524, 1184.1122, -0.0725
+            [INFO] [1505965752.701056]: new pose: 1323.3524, 1184.1122, 0.9993
+            [INFO] [1505965752.701745]: old pose: 1323.6859, 1184.0783, -0.0725
+            [INFO] [1505965752.702126]: new pose: 1323.6859, 1184.0783, 0.9993
+            [INFO] [1505965752.702907]: old pose: 1324.0195, 1184.0443, -0.0725
+            [INFO] [1505965752.703246]: new pose: 1324.0195, 1184.0443, 0.9993
+
+            '''
             nextx.append(xpoint)
             nexty.append(ypoint)
             # make one waypoint
             p = Waypoint()
-            p.pose.pose.position.x = float(xpoint)
-            p.pose.pose.position.y = float(ypoint)
-            p.pose.pose.position.z = float(0.0)
-            newyaw = math.atan2(ypoint - prevy, xpoint - prevx)
-            if newyaw < 0:
-                newyaw = newyaw + 2 * np.pi
-            q = tf.transformations.quaternion_from_euler(0.0, 0.0, newyaw)
+            #p.pose.pose.position.x = float(xpoint)
+            #p.pose.pose.position.y = float(ypoint)
+            #p.pose.pose.position.z = float(0.0)
+            p.pose = newPose
+            #newyaw = math.atan2(ypoint - prevy, xpoint - prevx)
+            #if newyaw < 0:
+            #    newyaw = newyaw + 2 * np.pi
+            q = tf.transformations.quaternion_from_euler(0.0, 0.0, newPose.pose.orientation.z)
             p.pose.pose.orientation = Quaternion(*q)
             p.pose.header.frame_id = "/world"
             p.twist.header.frame_id = '/world'
             #newvel = np.sqrt((prevx - xpoint)**2 + (prevy - ypoint)**2)/dt
             #p.twist.twist.linear.x = float(newvel)
             p.twist.twist.linear.x = float(self.ref_vel)
+            p.twist.twist.angular.z = newPose.pose.orientation.z
             waypoints.append(p)
 
             prevx = xpoint
